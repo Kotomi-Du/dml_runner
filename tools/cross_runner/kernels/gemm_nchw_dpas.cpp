7ff9d8d2a53e3fcc783439a967adefcfa2dbae09
Yaru Du <yaru.du@intel.com>
2023-11-21 11:17:13 +0800

    SLM related

--- tools/cross_runner/kernels/gemm_nchw_dpas.cpp
+++ tools/cross_runner/kernels/gemm_nchw_dpas.cpp
@@ -10,6 +10,9 @@
 #define SIZE_OF_HF16_BYTE 2
 #define SIZE_PER_DPAS_HF16 128  // DPAS works for half float matrix [8x16] [16x8]
 
+// SLM optimization
+#define SLM_KN_SHARING 1
+
 _GENX_ inline void myDPAS8(matrix_ref<HALF, 8, 16> matA,
                             matrix_ref<HALF, 8, 16> matB,
                             matrix_ref<FLOAT, 8, 8> result)
@@ -24,7 +27,7 @@ extern "C" _GENX_MAIN_ void gemm_nchw_dpas(
 )
 {
 #if !defined(EMPTY)
-    // TILE SIZE of 8M x 32N x 16K [tile_height x tile_width x step size]
+
 	// use byte to calcute offset
 	uint gidX = cm_group_id(DIM_X);
 	uint gidY = cm_group_id(DIM_Y);
@@ -35,27 +38,64 @@ extern "C" _GENX_MAIN_ void gemm_nchw_dpas(
 
 	const uint32_t thread_id_0 = cm_group_id(0) * cm_local_size(0) + cm_local_id(0);
 	const uint32_t thread_id_1 = cm_group_id(1) * cm_local_size(1) + cm_local_id(1);
-	const unsigned base_offset_a =  thread_id_0 * TILE_M * SIZE_K * SIZE_OF_HF16_BYTE;
-	const unsigned base_offset_b =  thread_id_1 * TILE_N * SIZE_OF_HF16_BYTE;
-	const unsigned base_offset_output =  (thread_id_0 * TILE_M * SIZE_N + thread_id_1 * TILE_N) * SIZE_OF_HF16_BYTE;
+	const uint32_t thread_id_2 = cm_group_id(2) * cm_local_size(2) + cm_local_id(2);
+	const uint32_t base_offset_a =  thread_id_0 * TILE_M * SIZE_K * SIZE_OF_HF16_BYTE;
+	const uint32_t base_offset_b =  thread_id_1 * TILE_N * SIZE_OF_HF16_BYTE;
+	const uint32_t base_offset_output =  (thread_id_0 * TILE_M * SIZE_N + thread_id_1 * TILE_N) * SIZE_OF_HF16_BYTE;
     
-	vector<HALF, SIZE_PER_DPAS_HF16 * TILE_M / 8> readA = 0.0; 	// M=0..7,  K=0..15		//A matrix format: [K/16][M][16k] : A tile: 8Mx16K
-	vector<HALF, SIZE_PER_DPAS_HF16> readB = 0.0; 	// N=0..7,  K=0..15		//B matrix format: [K/16][N/8][8K][8N][2K]	//B tile: 40Nx16K
-
-	//referrence variables	
+	//printf("%d,%d,%d\n", thread_id_0, thread_id_1, thread_id_2);
+	// init TILE_A
+	vector<HALF, SIZE_PER_DPAS_HF16 * TILE_M / 8> readA = 0.0; 	// M=0..7,  K=0..15		// A tile: (8*TILE_M/8)M x 16K
 	matrix_ref<HALF, TILE_M, 16> readA_m = readA.format<HALF, TILE_M, 16>();
-	matrix_ref<HALF, 8, 16> readB_m = readB.format<HALF, 8, 16>();
 
+	// init TILE_B
+	/** solution 1 start*/
+	// vector<HALF, SIZE_PER_DPAS_HF16> readB = 0.0; 	// N=0..7,  K=0..15		//B tile: 16Kx8N
+	// matrix_ref<HALF, 8, 16> readB_m = readB.format<HALF, 8, 16>();
+	/** solution 1 end*/
+	vector<HALF, SIZE_PER_DPAS_HF16 * TILE_N / 8 > readB = 0.0; 	// N=0..7,  K=0..15		//B tile: 16Kx8N
+	matrix_ref<HALF, 8, TILE_N*2> readB_m = readB.format<HALF, 8, TILE_N*2>();	// even though more registers are used here, the final registers are less than solution 1, and the perf is almost same
+	
 	//init the accumulators
 	matrix<FLOAT, TILE_M, TILE_N> result1 = 0.0;  
 	matrix_ref<FLOAT, TILE_M, TILE_N> result1ref = result1;
 
 
+#if SLM_KN_SHARING
+	cm_slm_init(TILE_K * TILE_N * SIZE_OF_HF16_BYTE);
+    uint slm_buffer = cm_slm_alloc(TILE_K * TILE_N * SIZE_OF_HF16_BYTE);
+#endif
+	
 	for( int step = 0; step < SIZE_K; step += TILE_K)
 	{
-		const unsigned step_base_offset_a = base_offset_a + step * SIZE_OF_HF16_BYTE;
-		const unsigned step_base_offset_b = base_offset_b + (step / TILE_K) * SIZE_N * TILE_K * SIZE_OF_HF16_BYTE;
+		const uint32_t step_base_offset_a = base_offset_a + step * SIZE_OF_HF16_BYTE;
+		const uint32_t step_base_offset_b = base_offset_b + (step / TILE_K) * SIZE_N * TILE_K * SIZE_OF_HF16_BYTE;
+		// TILE_B: read two lines, and ordered into DPAS required format
+#if SLM_KN_SHARING	
+		const uint32_t read_offset_b = step_base_offset_b + cm_local_id(0) * SIZE_N * 2 * SIZE_OF_HF16_BYTE;
+		vector<uint32_t, TILE_N/2> packed_rowX2_0;
+		vector<uint32_t, TILE_N/2> packed_rowX2_1;
 		
+		packed_rowX2_0.select<TILE_N/2,1>(0) = cm_load<U32, TILE_N/2, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_b, read_offset_b );  
+		packed_rowX2_1.select<TILE_N/2,1>(0) = cm_load<U32, TILE_N/2, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_b, read_offset_b + SIZE_N * SIZE_OF_HF16_BYTE);  
+
+        cm_slm_fence(CM_GLOBAL_COHERENT_FENCE);
+		cm_barrier();
+		const uint32_t slm_write_base_offset = cm_local_id(0) * TILE_N * 2 * SIZE_OF_HF16_BYTE;
+		cm_store_slm<uint32_t, TILE_N/2>(slm_write_base_offset, packed_rowX2_0.select<TILE_N/2, 1>(0));
+		cm_store_slm<uint32_t, TILE_N/2>(slm_write_base_offset + TILE_N * SIZE_OF_HF16_BYTE, packed_rowX2_1.select<TILE_N/2, 1>(0));
+		cm_slm_fence(CM_GLOBAL_COHERENT_FENCE);
+		cm_barrier();
+
+		#pragma unroll
+		for (int row = 0; row < 8; row++)
+		{
+			const uint32_t slm_read_base_offset = row * TILE_N * 2 * SIZE_OF_HF16_BYTE;
+			readB_m.select<1,1,TILE_N,2>(row, 0).format<U32>()= cm_load_slm<uint32_t, TILE_N/2>(slm_read_base_offset);
+			readB_m.select<1,1,TILE_N,2>(row, 1).format<U32>()= cm_load_slm<uint32_t, TILE_N/2>(slm_read_base_offset + TILE_N * SIZE_OF_HF16_BYTE);
+		}
+#else
+
 		matrix<HALF, 8, TILE_N> rowX2_0 = 0.0;  
 		matrix<HALF, 8, TILE_N> rowX2_1 = 0.0;
 
@@ -63,12 +103,14 @@ extern "C" _GENX_MAIN_ void gemm_nchw_dpas(
 		#pragma unroll
 		for(int row = 0; row < 8; row++)
 		{
-			const unsigned rowX2 = row * 2;
-			const unsigned read_offset_b = step_base_offset_b + (rowX2 * SIZE_N)* SIZE_OF_HF16_BYTE;
+			const uint32_t rowX2 = row * 2;
+			const uint32_t read_offset_b = step_base_offset_b + (rowX2 * SIZE_N)* SIZE_OF_HF16_BYTE;
 			rowX2_0.select<1,1,TILE_N,1>(row,0).format<U32>() = cm_load<U32, TILE_N/2, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_b, read_offset_b);  
 			rowX2_1.select<1,1,TILE_N,1>(row,0).format<U32>() = cm_load<U32, TILE_N/2, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_b, read_offset_b + SIZE_N* SIZE_OF_HF16_BYTE);  
-		}	
-		
+		}		
+#endif
+
+
 		#pragma unroll
 		for(int m=0; m < TILE_M/8; m++)
 		{
@@ -83,42 +125,47 @@ extern "C" _GENX_MAIN_ void gemm_nchw_dpas(
 			}	
 			
 			//calcute DPAS
+			
+ 
+			#pragma unroll	
 			for(int n = 0; n < TILE_N/8; n++)  
 			{
-				#pragma unroll
-				for (int row = 0; row < 8; row++)
-				{
-					readB_m.select<1,1,8,2>(row, 0)= rowX2_0.select<1,1,8,1>(row,8*n);
-					readB_m.select<1,1,8,2>(row, 1)= rowX2_1.select<1,1,8,1>(row,8*n);
-				}
-				
-				myDPAS8(readA_m.select<8,1,16,1>(m * 8, 0), readB_m, result1ref.select<8,1,8,1>(m * 8,n * 8));  
+				#if SLM_KN_SHARING
+					myDPAS8(readA_m.select<8,1,16,1>(m * 8, 0), readB_m.select<8,1,16,1>(0, n*16),result1ref.select<8,1,8,1>(m * 8,n * 8));
+				#else
+					#pragma unroll
+					for (int row = 0; row < 8; row++)
+					{
+						readB_m.select<1,1,8,2>(row, n*16)= rowX2_0.select<1,1,8,1>(row,8*n);
+						readB_m.select<1,1,8,2>(row, 1 + n*16)= rowX2_1.select<1,1,8,1>(row,8*n);		
+					}	
+					myDPAS8(readA_m.select<8,1,16,1>(m * 8, 0),  readB_m.select<8,1,16,1>(0, n*16), result1ref.select<8,1,8,1>(m * 8,n * 8));  
+				#endif
 			}
-
 		}
-		
 
-		// if (gidX == 0 && gidY == 0 && gidZ == 0)
+		// if (thread_id_0 == 4 && thread_id_1 == 0)
 		// {
-		// 	for (int i = 0; i < 8; i++)
+		// 	for (int i = 0; i < TILE_M; i++)
     	// 	{
         // 		  printf(" row%d", i);
-       	// 		 for (int j = 8; j < 8*2;j++)
+       	// 		 for (int j = 0; j < TILE_N;j++)
         // 		{ 
         // 		    printf(" %f", result1ref(i , j));
         // 		}
         // 		printf("\n");\
     	// 	}
 		// }
-		// if (gidX == 0 && gidY == 0 && gidZ == 0)
+		
+		// if (thread_id_0 == 15 && thread_id_1 == 159)
 		// {
 		// 	for (int i = 0; i < 8; i++)
     	// 	{
         // 		  printf(" row%d", i);
        	// 		 for (int j = 0; j < 16;j++)
         // 		{ 
-        // 		    printf(" %f",readA2(i *16+j));
-		// 			//printf(" %f",readB1(i *16+j));
+        // 		    //printf(" %f",readA2(i *16+j));
+		// 			printf(" %f",readB(i *16+j));
         // 		}
         // 		printf("\n");\
     	// 	}
